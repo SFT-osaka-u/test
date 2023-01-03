5bf7887f92bf40553ce4e740ec42b14c82ef4769
SFT-osaka-u <99697703+SFT-osaka-u@users.noreply.github.com>
2023-01-03 04:24:15 +0000

    test: add js

--- data.js
+++ data.js
@@ -1,1 +1,11 @@
-"\n  const bookData = {\n    isbn: 9870000000000,\n    title: \"aiueo\",\n    author: \"xyz\",\n    genre: \"math\",\n    stock: 3,\n    add: \"add\",\n    add2: \"add2\"\n  }\n  "
+
+  const bookData = {
+    isbn: 9870000000000,
+    title: "aiueo",
+    author: "xyz",
+    genre: "math",
+    stock: 3,
+    add: "add",
+    add2: "add2"
+  }
+  
